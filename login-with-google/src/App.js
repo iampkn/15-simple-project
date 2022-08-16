a31bcea6bfe4f7c3d2cce8263268c0dee65cfb23
HzViper <nguyenpham430hf@gmail.com>
2022-08-16 17:15:30 +0700

    update login with google

---
+++ login-with-google/src/App.js
@@ -0,0 +1,17 @@
+import {useEffect} from "react"
+require('dotenv').config
+
+
+function App() {
+useEffect(()=>{
+console.log("tutu")
+},[])
+  
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
