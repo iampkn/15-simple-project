42df88cc47c3da0faafb5d6041f998675fb5b18f
HzViper <nguyenpham430hf@gmail.com>
2022-08-17 14:53:30 +0700

    hide the env file

--- react-router/src/App.tsx
+++ react-router/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import Home from "./pages/Home"
 import Contact from "./pages/Contact"
 import News from "./pages/News"
@@ -7,7 +7,24 @@ import News from "./pages/News"
 function App() {
   return (
     <div className="App">
-      <h1>Router</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/news'>News</Link>
+          </li>
+          <li>
+            <Link to='/contact'>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/news' element={<News/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
     </div>
   );
 }
